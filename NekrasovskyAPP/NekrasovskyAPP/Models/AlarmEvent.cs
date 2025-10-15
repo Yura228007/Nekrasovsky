f60f2ab602d07bec08866464e25ff69acecbbf72
Yura Schedrin <iuriishchedrin@gmail.com>
2025-10-16 00:17:53 +0300

    Update Models

--- NekrasovskyAPP/NekrasovskyAPP/Models/AlarmEvent.cs
+++ NekrasovskyAPP/NekrasovskyAPP/Models/AlarmEvent.cs
@@ -9,7 +9,7 @@ namespace PasswordManagerV1.Models
     public class AlarmEvent
     {
         public int Id { get; set; }
-        public int EmployeeId { get; set; }
+        public int UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public string? Message { get; set; }
         public string Location { get; set; }

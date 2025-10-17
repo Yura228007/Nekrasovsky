a2f70d397cf33d07da914676e9395acd98419ffa
Yura Schedrin <iuriishchedrin@gmail.com>
2025-10-18 01:38:47 +0300

    Update DataBase

---
+++ NekrasovskyAPP/NekrasovskyAPP/Models/AccessibleMovement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NekrasovskyAPP.Models
+{
+    public class AccessibleMovement
+    {
+        public int FromWarehouseId { get; set; }
+        public int ToWarehouseId { get;set; }
+        public int MaterialId { get; set; }
+
+    }
+}
---
+++ NekrasovskyAPP/NekrasovskyAPP/Models/UserPermissions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace NekrasovskyAPP.Models
+{
+    public class UserPermissions
+    {
+        public int UserId { get; set; }
+        public int PermissionId { get; set; }
+    }
+}

49c1782af390160963dd4b209871111ac30f80b9
Yura Schedrin <iuriishchedrin@gmail.com>
2025-11-07 00:13:35 +0300

    Add Server

---
+++ server/server/Models/UploadedFile.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Models;
+
+public class UploadedFile
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public long FileSize { get; set; }
+    public DateTime UploadDate { get; set; } = DateTime.UtcNow;
+}

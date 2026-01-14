419e2e58e6eae59e5ef513b57c4f00c251fc21d0
Yukimura1000-7 <156335735+Yukimura1000-7@users.noreply.github.com>
2026-01-14 17:07:05 +0400

    Now the app does not crash when the apk is launched

--- server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa5b214f1bfaaf06525480c0124ab3820b6c6aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f423b36e103854db5d3be3d223a925ad51661e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
